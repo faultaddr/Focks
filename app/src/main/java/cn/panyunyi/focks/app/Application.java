6f5509f50a1a2e8b0fcef07bc941ac9f803eb078
潘云逸 <panyunyi@meituan.com>
2020-05-22 22:42:17 +0800

    Initial commit

---
+++ app/src/main/java/cn/panyunyi/focks/app/Application.java
@@ -0,0 +1,4 @@
+package cn.panyunyi.focks.app;
+
+public class Application extends android.app.Application {
+}
---
+++ app/src/main/java/cn/panyunyi/focks/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.panyunyi.focks.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cn.panyunyi.focks.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
