b68788f60f27e810e4f5e64fdbe84314b274ff8a
潘云逸 <panyunyi@meituan.com>
2020-05-24 17:33:42 +0800

    主页面设计与构建
    N/A
    
    主页面设计与构建
    
    Signed-off-by: 潘云逸 <panyunyi@meituan.com>

--- app/src/main/java/cn/panyunyi/focks/app/Application.java
+++ app/src/main/java/cn/panyunyi/focks/app/Application.java
@@ -1,4 +1,12 @@
 package cn.panyunyi.focks.app;
 
+import java.util.List;
+
 public class Application extends android.app.Application {
+    private final static String TAG="Application";
+    public static List<Application>mApplicationInfos;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
